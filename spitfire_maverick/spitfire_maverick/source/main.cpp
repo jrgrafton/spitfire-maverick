a17ee92669ebd77d0436846740b0d8a8cb0385a3
jamesgrafton <jamesgrafton@9e128ace-5a45-0410-969d-bf5d6f0db603>
2008-02-04 20:37:07 +0000

    --Test
    git-svn-id: http://svn.jamesgrafton.com/spitfire-maverick/trunk@2 9e128ace-5a45-0410-969d-bf5d6f0db603

--- spitfire_maverick/spitfire_maverick/source/main.cpp
+++ spitfire_maverick/spitfire_maverick/source/main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
 	PA_Init();    // Initializes PA_Lib
-	PA_InitVBL(); // Initializes a standard VBL
+	PA_InitVBL(); //// Initializes a standard VBL
 
 	PA_InitText(1, 2);
 	PA_OutputSimpleText(1, 1, 2, "Hello World!");
